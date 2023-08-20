bb2827dacaaa44a2654da1e5ee7f959c921ba496
Володимир Nagual <nahualli.ya@gmail.com>
2023-08-20 16:48:31 +0300

    add Message class

---
+++ Signal_ChatR_WebApi/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Signal_ChatR_WebApi.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int RoomId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        public string? MsgText { get; set; }
+        public string? MsgFilePath { get; set;}
+        public string? MsgFileMime { get; set; }
+    }
+}
