0fd8f7a2dfd064a55cdb44851459a7257f96c5fb
Володимир Nagual <nahualli.ya@gmail.com>
2023-09-09 21:30:20 +0300

    Login form create

--- signal_chatr_web/src/Components/Login.jsx
+++ signal_chatr_web/src/Components/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+
+  const [userData, setUserData] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setUserData(values => ({ ...values, [name]: value }));
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(userData);
+  }
+
   return (
       <div>
-          <form >
+          <form onSubmit={handleSubmit} >
+            <div className='form-group'>
+              <label htmlFor="email">Email</label>
+              <input type="text" name='email' onChange={handleChange} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor="password" className='form-label'>Email</label>
+              <input type="password" name='password' className='form-control' onChange={handleChange} />
+            </div>
+            <div className='form-group'>
+              <input type="submit" className='btn btn-success' value={"Login"} />
+            </div>
           </form>
       </div>
   );
