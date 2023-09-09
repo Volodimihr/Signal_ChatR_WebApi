f93156e98976c7c9789d353aca44c155856fa192
Володимир Nagual <nahualli.ya@gmail.com>
2023-09-09 16:23:33 +0300

    Add React standalone project

--- Signal_ChatR_WebApi/Controllers/UsersController.cs
+++ Signal_ChatR_WebApi/Controllers/UsersController.cs
@@ -15,7 +15,7 @@ namespace Signal_ChatR_WebApi.Controllers
             _context = context;
         }
 
-        [HttpPost("login/{email}{password}")]
+        [HttpPost("login")]
         public async Task<ActionResult<User>> Login(string email, string password)
         {
             if (_context.Users == null)
@@ -97,7 +97,7 @@ namespace Signal_ChatR_WebApi.Controllers
 
         // POST: api/Users
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
+        [HttpPost("register")]
         public async Task<ActionResult<User>> PostUser(User user)
         {
             if (_context.Users == null)
