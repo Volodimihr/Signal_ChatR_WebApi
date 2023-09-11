494ba3184a3425fa8ed01c9087ecfc8b86c1a99a
Володимир Nagual <nahualli.ya@gmail.com>
2023-09-12 02:30:41 +0300

    import components to chat component

--- signal_chatr_web/src/Components/Chat.jsx
+++ signal_chatr_web/src/Components/Chat.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Header from 'Header';
+import Msgsarea from 'Msgsarea';
+import Sender from 'Sender';
+import Rooms from 'Rooms';
 
 export default function Chat({ userId }) {
 
@@ -7,10 +11,14 @@ export default function Chat({ userId }) {
         userId(null);
     }
 
-  return (
-      <div>
-          <h1>Chat</h1>
-          <input type='button' onClick={logOutHandle} value='Logout' />
-      </div>
-  )
+    return (
+        <div>
+            <h1>Chat</h1>
+            <input type='button' onClick={logOutHandle} value='Logout' />
+            <Header />
+            <Msgsarea />
+            <Sender />
+            <Rooms />
+        </div>
+    )
 }
