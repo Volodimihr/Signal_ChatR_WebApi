1c29f9ec532848f7d4fb47493dcfdfd01fcd0e0c
Володимир Nagual <nahualli.ya@gmail.com>
2023-09-30 23:19:52 +0300

    Create Signal CharR WPF Project - clean project

---
+++ Signal_ChatR_WPF/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Signal_ChatR_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
